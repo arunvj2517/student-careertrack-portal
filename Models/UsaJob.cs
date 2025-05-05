5d58f61c5bd07b5f1381e7e17c6d2d5bbc170144
yamunayoga <yoganandayamuna@gmail.com>
2025-05-06 03:05:55 +0530

    Added CRUD Module's MVC

---
+++ Models/UsaJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace careerPortal.Models
+{
+    public class UsaJob
+    {
+        public string PositionId { get; set; } = Guid.NewGuid().ToString();
+
+        public string PositionTitle { get; set; }
+
+        public string OrganizationName { get; set; }
+
+        public string Location { get; set; }
+
+        public string SalaryRange { get; set; }
+
+        public string PositionUri { get; set; }
+
+        public DateTime PublicationStartDate { get; set; }
+
+        public bool IsFromApi { get; set; }
+
+        public string? Status { get; set; }
+
+        public DateTime DateSaved { get; set; }
+    }
+}
